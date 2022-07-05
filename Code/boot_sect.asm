dbc5684843d0fa6784092e2e7a5528f43d574e8f
SimonTha <st.simon.tha@gmail.com>
2022-07-05 22:17:12 +0200

    Disk and Print Function
    Added sophisticated print String and print Hex funtion
    Added support for reading disk outside the bootsector

--- Code/boot_sect.asm
+++ Code/boot_sect.asm
@@ -1,27 +1,37 @@
-mov ah, 0x0e
-mov al, "H"
-int 0x10
-mov al, "e"
-int 0x10
-mov al, "l"
-int 0x10
-mov al, "l"
-int 0x10
-mov al, "o"
-int 0x10
-mov al, " "
-int 0x10
-mov al, "W"
-int 0x10
-mov al, "o"
-int 0x10
-mov al, "r"
-int 0x10
-mov al, "l"
-int 0x10
-mov al, "d"
-int 0x10
-jmp $
+[org 0x7c00]            ;Specifie address for orientation
 
-times 510-($-$$) db 0
-dw 0xaa55
+mov [BootDrive], dl     ;Save boot drive in BootDrive
+
+mov bp, 0x8000
+mov sp, bp              ;Sets the basepointer of the Stack
+
+mov bx, 0x9000
+mov dh, 5
+mov dl, [BootDrive]
+call readDisk           ;Test code for showing disk reading
+
+mov bx, [0x9000]
+call printHex           ;Prove that disk got read and print it
+
+mov bx, 0x9000
+call printStr           ;Print it as String for fun :)
+
+
+jmp $                   ;Loop forever
+
+;Include "libraries" or more completed code
+%include "libs/print.asm"
+%include "libs/diskLoader.asm"
+
+;Global Variables
+testMsg:
+  db "Hello World", 0
+
+BootDrive:
+  db 0
+
+times 510-($-$$) db 0     ; Fill the Bootsector with 0's
+dw 0xaa55                 ;"Magic Number": End of Bootsector
+
+times 256 dw 0xdada       ;Store data outside the Bootsector for testing and bootstrapping
+times 256 dw 0xface       ;**
---
+++ Code/libs/diskLoader.asm
@@ -0,0 +1,28 @@
+;readDisk: This function reads the Disk via BIOS and stores it in memory
+;Parameters: bx = where the read Data is going to be stored; dh = How many sectors are going to be read;
+
+readDisk:
+ push dx
+
+ ;Set segment registers
+ mov ah, 0x02
+ mov al, dh
+ mov ch, 0x00
+ mov dh, 0x00
+ mov cl, 0x02
+
+ int 0x13         ;BIOS Interrupt for reading the DISK
+ jc diskError     ;Error flag triggerd if BIOS isn't able to read the DISK
+
+ pop dx
+ cmp dh, al       ;Check if the correct sectors got read
+ jne diskError
+ ret
+
+diskError:
+  mov bx, errorMsg
+  call printStr
+  jmp $
+
+errorMsg:
+  db "ERROR: Not able to read Disk", 0
